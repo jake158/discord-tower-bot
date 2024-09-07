abfa2200aa3f891f070c9372d1cad5cc9a670928
freeram <kulikovpavel2801@gmail.com>
2024-09-06 19:01:08 -0600

    Add boilerplate

---
+++ Tower/Program.cs
@@ -0,0 +1,35 @@
+﻿using Discord;
+using Discord.WebSocket;
+
+namespace Tower;
+public class Program
+{
+    private static DiscordSocketClient _client = new();
+
+    public static async Task Main()
+    {
+        _client.Log += Log;
+
+        //  You can assign your bot token to a string, and pass that in to connect.
+        //  This is, however, insecure, particularly if you plan to have your code hosted in a public repository.
+        var token = "token";
+
+        // Some alternative options would be to keep your token in an Environment Variable or a standalone file.
+        // var token = Environment.GetEnvironmentVariable("NameOfYourEnvironmentVariable");
+        // var token = File.ReadAllText("token.txt");
+        // var token = JsonConvert.DeserializeObject<AConfigurationClass>(File.ReadAllText("config.json")).Token;
+
+        await _client.LoginAsync(TokenType.Bot, token);
+        await _client.StartAsync();
+
+        // Block this task until the program is closed.
+        await Task.Delay(-1);
+    }
+
+
+    private static Task Log(LogMessage msg)
+    {
+        Console.WriteLine(msg.ToString());
+        return Task.CompletedTask;
+    }
+}
