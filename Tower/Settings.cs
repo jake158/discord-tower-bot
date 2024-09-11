3885216a67f3708256bf135b7cdf650b87bc3782
freeram <kulikovpavel2801@gmail.com>
2024-09-10 19:13:22 -0600

    Add loading token from config.json

---
+++ Tower/Settings.cs
@@ -0,0 +1,32 @@
+using System.Text.Json;
+
+namespace Tower;
+public class Settings
+{
+    public string? Token { get; init; }
+    private const string ConfigFileName = "config.json";
+
+    public static Settings Load()
+    {
+        try
+        {
+            if (!File.Exists(ConfigFileName))
+            {
+                throw new FileNotFoundException($"Configuration file '{ConfigFileName}' not found.");
+            }
+            var settings = JsonSerializer.Deserialize<Settings>(File.ReadAllText(ConfigFileName))
+            ?? throw new Exception("Failed to deserialize configuration file.");
+
+            if ((settings.Token == null) || string.IsNullOrEmpty(settings.Token))
+            {
+                throw new Exception($"Token not specified in '{ConfigFileName}'");
+            }
+            return settings;
+        }
+        catch (Exception ex)
+        {
+            Console.WriteLine($"Error loading configuration: {ex.Message}");
+            throw;
+        }
+    }
+}
