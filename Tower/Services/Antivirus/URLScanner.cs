c192b45a8fa210e91933888b830ed0a7e8817b2b
freeram <kulikovpavel2801@gmail.com>
2024-11-14 10:20:49 -0700

    Add scanning URLs with Web Risk API

--- Tower/Services/Antivirus/URLScanner.cs
+++ Tower/Services/Antivirus/URLScanner.cs
@@ -1,38 +1,117 @@
 using Microsoft.Extensions.Logging;
+using Google.Cloud.WebRisk.V1;
 using Tower.Services.Antivirus.Models;
+using Tower.Persistence.Entities;
+using Microsoft.Extensions.DependencyInjection;
+using System.ComponentModel.DataAnnotations;
 
 namespace Tower.Services.Antivirus;
 public class URLScanner
 {
     private readonly ILogger<URLScanner> _logger;
-    // private readonly HttpClient _httpClient;
-    // private readonly string _googleApiKey;
-    // public URLScanner(ILogger<URLScanner> logger, HttpClient httpClient, Settings settings)
-    public URLScanner(ILogger<URLScanner> logger)
+    private readonly IServiceScopeFactory _scopeFactory;
+    private readonly WebRiskServiceClient _webRiskClient;
+
+    public URLScanner(ILogger<URLScanner> logger, IServiceScopeFactory scopeFactory, WebRiskServiceClient webRiskClient)
     {
         _logger = logger;
-        // _httpClient = httpClient;
-        // _googleApiKey = settings.GoogleApiKey;
+        _scopeFactory = scopeFactory;
+        _webRiskClient = webRiskClient;
     }
 
-    public async Task<ScanResult> ScanUrlAsync(Uri webUri, CancellationToken cancellationToken)
+    public class URLScannerOptions
     {
-        if (webUri.IsFile)
+        [Required]
+        public string GoogleServiceAccountJson { get; set; } = "";
+    }
+
+    public async Task<ScanResult> ScanUrlAsync(Uri url, CancellationToken cancellationToken)
+    {
+        _logger.LogDebug($"Checking cache for URL: {url.AbsoluteUri}");
+
+        using var scope = _scopeFactory.CreateScope();
+        var cache = scope.ServiceProvider.GetRequiredService<ScanResultCache>();
+
+        var cachedResult = await cache.GetScanResultAsync(url, ResourceType.WebLink);
+
+        if (cachedResult != null)
         {
-            throw new ArgumentException("The provided URI is a file, not a web URL", nameof(webUri));
+            _logger.LogInformation($"Cache hit for URL: {url.AbsoluteUri}");
+            return (ScanResult)cachedResult;
         }
 
-        _logger.LogInformation($"Scanning URL: {webUri.AbsoluteUri}");
+        _logger.LogInformation($"Cache miss for URL: {url.AbsoluteUri}. Calling Web Risk API");
 
-        // string requestUrl = $"https://webrisk.googleapis.com/v1/uris:search?uri={webUri.AbsoluteUri}&threatTypes=MALWARE,UNWANTED_SOFTWARE,SOCIAL_ENGINEERING&key={_googleApiKey}";
+        var apiResponse = await CallWebRiskApiAsync(url, cancellationToken);
 
-        // var response = await _httpClient.GetAsync(requestUrl);
-        // var content = await response.Content.ReadAsStringAsync();
+        _logger.LogInformation($"Saving scan result for URL: {url.AbsoluteUri} to cache");
 
-        // bool isMalicious = content.Contains("threatTypes");
+        return await cache.SaveScanResultAsync(
+            url: url,
+            type: ResourceType.WebLink,
+            isMalware: apiResponse.IsMalware,
+            isSuspicious: apiResponse.IsSuspicious,
+            expireTime: apiResponse.ExpireTime
+        );
+    }
 
-        bool isMalicious = false;
+    private async Task<ApiResponse> CallWebRiskApiAsync(Uri url, CancellationToken cancellationToken)
+    {
+        var threatTypes = new[] { ThreatType.Malware, ThreatType.SocialEngineering, ThreatType.UnwantedSoftware };
 
-        return new ScanResult(webUri.AbsoluteUri, isMalware: isMalicious, isSuspicious: false);
+        SearchUrisResponse response;
+        try
+        {
+            response = await _webRiskClient.SearchUrisAsync(
+                uri: url.AbsoluteUri,
+                threatTypes: threatTypes,
+                cancellationToken
+                );
+        }
+        catch (Exception ex)
+        {
+            _logger.LogError(ex, $"Error calling Web Risk API for URL: {url}");
+            throw;
+        }
+        return ConstructApiResponse(response);
+    }
+
+    private static ApiResponse ConstructApiResponse(SearchUrisResponse response)
+    {
+        bool isMalware = false;
+        bool isSuspicious = false;
+        DateTimeOffset? expireTime = null;
+
+        if (response.Threat != null)
+        {
+            var threat = response.Threat;
+
+            if (threat.ThreatTypes.Contains(ThreatType.Malware) || threat.ThreatTypes.Contains(ThreatType.SocialEngineering))
+            {
+                isMalware = true;
+            }
+            if (threat.ThreatTypes.Contains(ThreatType.UnwantedSoftware))
+            {
+                isSuspicious = true;
+            }
+            if (threat.ExpireTime != null)
+            {
+                expireTime = threat.ExpireTime.ToDateTimeOffset();
+            }
+        }
+
+        return new ApiResponse
+        {
+            IsMalware = isMalware,
+            IsSuspicious = isSuspicious,
+            ExpireTime = expireTime
+        };
+    }
+
+    private readonly struct ApiResponse
+    {
+        public bool IsMalware { get; init; }
+        public bool IsSuspicious { get; init; }
+        public DateTimeOffset? ExpireTime { get; init; }
     }
 }
