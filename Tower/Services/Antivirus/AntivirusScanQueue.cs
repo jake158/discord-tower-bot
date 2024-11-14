c31186287a12bda1f2bc196231ee4cfa92043ad9
freeram <kulikovpavel2801@gmail.com>
2024-11-14 10:53:18 -0700

    Add capacity logging messages in scan queue

--- Tower/Services/Antivirus/AntivirusScanQueue.cs
+++ Tower/Services/Antivirus/AntivirusScanQueue.cs
@@ -1,18 +1,22 @@
 using System.ComponentModel.DataAnnotations;
 using System.Threading.Channels;
 using Microsoft.Extensions.Options;
+using Microsoft.Extensions.Logging;
 using Tower.Services.Antivirus.Models;
 
 namespace Tower.Services.Antivirus;
 public class AntivirusScanQueue : IAntivirusScanQueue
 {
+    private readonly ILogger<AntivirusScanQueue> _logger;
     private readonly Channel<ScanRequest> _queue;
+    private readonly int _capacity;
 
-    public AntivirusScanQueue(IOptions<AntivirusScanQueueOptions> options)
+    public AntivirusScanQueue(ILogger<AntivirusScanQueue> logger, IOptions<AntivirusScanQueueOptions> options)
     {
-        var capacity = options.Value.Capacity;
+        _logger = logger;
+        _capacity = options.Value.Capacity;
 
-        var boundedChannelOptions = new BoundedChannelOptions(capacity)
+        var boundedChannelOptions = new BoundedChannelOptions(_capacity)
         {
             FullMode = BoundedChannelFullMode.Wait
         };
@@ -31,6 +35,22 @@ public class AntivirusScanQueue : IAntivirusScanQueue
         {
             throw new ArgumentNullException(nameof(url));
         }
+        var currentCapacity = _queue.Reader.Count;
+
+        if (currentCapacity >= _capacity)
+        {
+            _logger.LogCritical("ScanQueue has reached full capacity: {CurrentCapacity}/{MaxCapacity}. Attempting to enqueue URL: {Url}", currentCapacity, _capacity, url);
+        }
+        else if (currentCapacity >= Math.Floor(_capacity * 0.8))
+        {
+            _logger.LogWarning("ScanQueue is nearing full capacity (80%): {CurrentCapacity}/{MaxCapacity}", currentCapacity, _capacity);
+        }
+        else if (currentCapacity >= Math.Floor(_capacity * 0.5))
+        {
+            _logger.LogWarning("ScanQueue is at 50% capacity: {CurrentCapacity}/{MaxCapacity}", currentCapacity, _capacity);
+        }
+
+        _logger.LogDebug("Attempting to enqueue URL: {Url}. Current queue usage: {CurrentCapacity}/{MaxCapacity}", url, currentCapacity, _capacity);
 
         var tcs = new TaskCompletionSource<ScanResult>();
         var scanRequest = new ScanRequest(url, isFile, tcs);
