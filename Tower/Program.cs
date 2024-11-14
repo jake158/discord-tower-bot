24744baeb21f5175dbfe9259994a3e0fae40bf72
freeram <kulikovpavel2801@gmail.com>
2024-11-14 10:21:36 -0700

    Move service configuration to separate file, add Google Web Risk setup

--- Tower/Program.cs
+++ Tower/Program.cs
@@ -1,15 +1,9 @@
-﻿using Discord;
-using Discord.Interactions;
-using Discord.WebSocket;
-using Microsoft.Data.SqlClient;
-using Microsoft.EntityFrameworkCore;
+﻿using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
 using Microsoft.Extensions.Logging;
 using Tower.Persistence;
-using Tower.Services.Antivirus;
-using Tower.Services.Discord;
 
 namespace Tower;
 internal sealed class Program
@@ -17,114 +11,46 @@ internal sealed class Program
     public static async Task Main(string[] args)
     {
         var host = Host.CreateDefaultBuilder(args)
-        .ConfigureHostConfiguration(configurationBuilder =>
-        {
-            configurationBuilder.AddCommandLine(args);
-        })
-        .ConfigureAppConfiguration((context, config) =>
-        {
-            var env = context.HostingEnvironment;
-            Console.WriteLine($"Running in environment: {env.EnvironmentName}");
-
-            config.AddEnvironmentVariables();
-
-            if (env.IsDevelopment())
+            .ConfigureHostConfiguration(configurationBuilder =>
             {
-                config.AddUserSecrets<Program>(true);
-            }
-
-            // if (env.IsProduction())
-            // {
-            //     var builtConfig = config.Build();
-            //     var keyVaultEndpoint = new Uri(builtConfig["KeyVault:VaultUri"]);
-            //     var credential = new DefaultAzureCredential();
-            //     config.AddAzureKeyVault(keyVaultEndpoint, credential);
-            // }
-        })
-        .ConfigureServices((context, services) =>
-        {
-            var config = context.Configuration;
-
-            services.AddDbContext<TowerDbContext>(options =>
+                configurationBuilder.AddCommandLine(args);
+            })
+            .ConfigureAppConfiguration((context, config) =>
             {
-                var connectionString = config.GetConnectionString("DefaultConnection");
-                var password = config["SqlServerPassword"] ?? Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
-                var sqlServerHost = Environment.GetEnvironmentVariable("SQL_SERVER_HOST") ?? "localhost";
-                var sqlServerPort = config["SqlServerPort"] ?? Environment.GetEnvironmentVariable("SQL_SERVER_PORT") ?? "1433";
+                var env = context.HostingEnvironment;
+                Console.WriteLine($"Running in environment: {env.EnvironmentName}");
 
-                var conStrBuilder = new SqlConnectionStringBuilder(connectionString)
+                config.AddEnvironmentVariables();
+
+                if (env.IsDevelopment())
                 {
-                    Password = password,
-                    DataSource = $"{sqlServerHost},{sqlServerPort}"
-                };
-                var connection = conStrBuilder.ConnectionString;
+                    config.AddUserSecrets<Program>(true);
+                }
 
-                options.UseSqlServer(connection);
-            });
-
-
-            int antivirusQueueCapacity = config.GetValue<int>("Antivirus:QueueCapacity");
-            Console.WriteLine($"Antivirus queue capacity: {antivirusQueueCapacity}");
-
-            services
-                .AddSingleton<IAntivirusScanQueue>(new AntivirusScanQueue(antivirusQueueCapacity))
-                .AddScoped<ScanResultCache>()
-                .AddSingleton<FileScanner>()
-                .AddSingleton<URLScanner>();
-
-            // services
-            //     .AddOptions<URLScanner.URLScannerOptions>()
-            //     .Configure<IConfiguration>((options, config) =>
-            //     {
-            //         var googleApiKey = config["GoogleApiKey"] ?? Environment.GetEnvironmentVariable("GOOGLE_API_KEY");
-            //         ArgumentNullException.ThrowIfNull(googleApiKey, nameof(googleApiKey));
-            //         options.GoogleApiKey = googleApiKey;
-            //     });
-
-            services.AddHostedService<AntivirusService>();
-
-
-            var discordSocketConfig = new DiscordSocketConfig()
+                // if (env.IsProduction())
+                // {
+                //     var builtConfig = config.Build();
+                //     var keyVaultEndpoint = new Uri(builtConfig["KeyVault:VaultUri"]);
+                //     var credential = new DefaultAzureCredential();
+                //     config.AddAzureKeyVault(keyVaultEndpoint, credential);
+                // }
+            })
+            .ConfigureServices((context, services) =>
             {
-                GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
-            };
+                var config = context.Configuration;
 
-            var interactionServiceConfig = new InteractionServiceConfig()
+                services
+                    .AddDatabase(config)
+                    .AddGoogleWebRisk(config)
+                    .AddAntivirusServices(config)
+                    .AddDiscordServices(config);
+            })
+            .ConfigureLogging(logging =>
             {
-                UseCompiledLambda = true
-            };
-
-            services
-                .AddScoped<BotDatabaseManager>()
-                .AddSingleton(discordSocketConfig)
-                .AddSingleton<DiscordSocketClient>()
-                .AddSingleton<DiscordLogHandler>()
-                .AddSingleton<MessageHandler>()
-                .AddSingleton<InteractionService>(serviceProvider =>
-                {
-                    var client = serviceProvider.GetRequiredService<DiscordSocketClient>();
-                    return new InteractionService(client.Rest, interactionServiceConfig);
-                });
-
-            services
-                .Configure<FileScanner.FileScannerOptions>(config.GetSection("AntivirusServer"))
-                .AddOptions<BotService.BotServiceOptions>()
-                .Configure<IConfiguration>((options, config) =>
-                {
-                    var token = config["DiscordToken"] ?? Environment.GetEnvironmentVariable("DISCORD_TOKEN");
-                    ArgumentNullException.ThrowIfNull(token, nameof(token));
-                    options.Token = token;
-                    options.TestGuildID = config.GetValue<ulong?>("Discord:TestGuildID");
-                });
-
-            services.AddHostedService<BotService>();
-        })
-        .ConfigureLogging(logging =>
-        {
-            logging.ClearProviders();
-            logging.AddConsole();
-        })
-        .Build();
+                logging.ClearProviders();
+                logging.AddConsole();
+            })
+            .Build();
 
         using (var scope = host.Services.CreateScope())
         {
---
+++ Tower/ServiceCollectionExtensions.cs
@@ -0,0 +1,116 @@
+using Discord;
+using Discord.Interactions;
+using Discord.WebSocket;
+using Google.Apis.Auth.OAuth2;
+using Google.Cloud.WebRisk.V1;
+using Grpc.Auth;
+using Microsoft.Data.SqlClient;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.Configuration;
+using Microsoft.Extensions.DependencyInjection;
+using Tower.Persistence;
+using Tower.Services.Antivirus;
+using Tower.Services.Discord;
+
+namespace Tower;
+public static class ServiceCollectionExtensions
+{
+    public static IServiceCollection AddDatabase(this IServiceCollection services, IConfiguration config)
+    {
+        services.AddDbContext<TowerDbContext>(options =>
+        {
+            var connectionString = config.GetConnectionString("DefaultConnection");
+            var password = config["SqlServerPassword"] ?? Environment.GetEnvironmentVariable("SQL_SERVER_PASSWORD");
+            var sqlServerHost = Environment.GetEnvironmentVariable("SQL_SERVER_HOST") ?? "localhost";
+            var sqlServerPort = config["SqlServerPort"] ?? Environment.GetEnvironmentVariable("SQL_SERVER_PORT") ?? "1433";
+
+            var conStrBuilder = new SqlConnectionStringBuilder(connectionString)
+            {
+                Password = password,
+                DataSource = $"{sqlServerHost},{sqlServerPort}"
+            };
+            options.UseSqlServer(conStrBuilder.ConnectionString);
+        });
+
+        return services;
+    }
+
+    public static IServiceCollection AddGoogleWebRisk(this IServiceCollection services, IConfiguration config)
+    {
+        string? googleJsonCredentials = config["GoogleServiceAccountJson"] ?? Environment.GetEnvironmentVariable("GOOGLE_SERVICE_ACCOUNT_JSON");
+        ArgumentNullException.ThrowIfNull(googleJsonCredentials, nameof(googleJsonCredentials));
+
+        var builder = new WebRiskServiceClientBuilder
+        {
+            ChannelCredentials = GoogleCredential.FromJson(googleJsonCredentials).ToChannelCredentials()
+        };
+
+        services.AddSingleton<WebRiskServiceClient>(builder.Build());
+        return services;
+    }
+
+    public static IServiceCollection AddAntivirusServices(this IServiceCollection services, IConfiguration config)
+    {
+        services
+            .Configure<FileScanner.FileScannerOptions>(config.GetSection("FileScanner"))
+            .PostConfigure<FileScanner.FileScannerOptions>(options =>
+            {
+                var envHost = Environment.GetEnvironmentVariable("ANTIVIRUS_SERVER_HOST");
+                if (!string.IsNullOrEmpty(envHost)) options.AntivirusServerHost = envHost;
+
+                var envPort = Environment.GetEnvironmentVariable("ANTIVIRUS_SERVER_PORT");
+                if (!string.IsNullOrEmpty(envPort) && int.TryParse(envPort, out var port)) options.AntivirusServerPort = port;
+
+                var envSharedDirectory = Environment.GetEnvironmentVariable("SHARED_DIRECTORY");
+                if (!string.IsNullOrEmpty(envSharedDirectory)) options.SharedDirectory = envSharedDirectory;
+            });
+
+        services
+            .Configure<AntivirusScanQueue.AntivirusScanQueueOptions>(config.GetSection("AntivirusScanQueue"))
+            .AddSingleton<IAntivirusScanQueue, AntivirusScanQueue>()
+            .AddScoped<ScanResultCache>()
+            .AddSingleton<FileScanner>()
+            .AddSingleton<URLScanner>()
+            .AddHostedService<AntivirusService>();
+
+        return services;
+    }
+
+    public static IServiceCollection AddDiscordServices(this IServiceCollection services, IConfiguration config)
+    {
+        var discordSocketConfig = new DiscordSocketConfig
+        {
+            GatewayIntents = GatewayIntents.AllUnprivileged | GatewayIntents.MessageContent
+        };
+
+        var interactionServiceConfig = new InteractionServiceConfig
+        {
+            UseCompiledLambda = true
+        };
+
+        services
+            .AddScoped<BotDatabaseManager>()
+            .AddSingleton(discordSocketConfig)
+            .AddSingleton<DiscordSocketClient>()
+            .AddSingleton<DiscordLogHandler>()
+            .AddSingleton<MessageHandler>()
+            .AddSingleton<InteractionService>(serviceProvider =>
+            {
+                var client = serviceProvider.GetRequiredService<DiscordSocketClient>();
+                return new InteractionService(client.Rest, interactionServiceConfig);
+            });
+
+        services
+            .AddOptions<BotService.BotServiceOptions>()
+            .Configure<IConfiguration>((options, config) =>
+            {
+                var token = config["DiscordToken"] ?? Environment.GetEnvironmentVariable("DISCORD_TOKEN");
+                ArgumentNullException.ThrowIfNull(token, nameof(token));
+                options.Token = token;
+                options.TestGuildID = config.GetValue<ulong?>("Discord:TestGuildID");
+            });
+
+        services.AddHostedService<BotService>();
+        return services;
+    }
+}
