9ba0c3d07f1eb2e1ed002d9a9ead26683caa1a75
jake158 <kulikovpavel2801@gmail.com>
2024-12-25 20:51:53 -0700

    Rename secrets and group them for clarity

--- run-dev.sh
+++ run-dev.sh
@@ -1,9 +1,9 @@
 #!/bin/bash
 
 secrets=$(dotnet user-secrets list -p Tower/)
-export SQL_SERVER_PORT=$(echo "$secrets" | grep "SqlServerPort" | awk -F' = ' '{print $2}')
-export SQL_SERVER_PASSWORD=$(echo "$secrets" | grep "SqlServerPassword" | awk -F' = ' '{print $2}')
-export DISCORD_TOKEN=$(echo "$secrets" | grep "DiscordToken" | awk -F' = ' '{print $2}')
+export DB_PORT=$(echo "$secrets" | grep "DatabaseSettings:Port" | awk -F' = ' '{print $2}')
+export DB_PASSWORD=$(echo "$secrets" | grep "DatabaseSettings:Password" | awk -F' = ' '{print $2}')
+export DISCORD_TOKEN=$(echo "$secrets" | grep "Discord:Token" | awk -F' = ' '{print $2}')
 
 # dotnet user-secrets set -p Tower/ "GoogleServiceAccountJson" "$(cat ./tower-web-risk.json | jq -c .)"
 export GOOGLE_SERVICE_ACCOUNT_JSON=$(echo "$secrets" | grep "GoogleServiceAccountJson" | awk -F' = ' '{print $2}')
